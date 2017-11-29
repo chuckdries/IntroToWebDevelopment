2c0bd85391d389ea72f5cdd0ff25936d4be52a24
Chuck Dries <chuck@chuckdries.com>
2017-11-29 07:58:06 -0700

    compile

--- FullStack/dist/dbtest.js
+++ FullStack/dist/dbtest.js
@@ -12,53 +12,77 @@ var _sqlite = require('sqlite');
 
 var _sqlite2 = _interopRequireDefault(_sqlite);
 
+var _nunjucks = require('nunjucks');
+
+var _nunjucks2 = _interopRequireDefault(_nunjucks);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new _bluebird2.default(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return _bluebird2.default.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+const app = (0, _express2.default)();
+const dbPromise = _sqlite2.default.open('./test.db', { Promise: _bluebird2.default });
 
-var app = (0, _express2.default)();
-var dbPromise = _sqlite2.default.open('./test.db', { Promise: _bluebird2.default });
+async function init() {
+    const db = await dbPromise;
+    await db.run("PRAGMA foreign_keys = ON;");
+}
 
-app.get('/', function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res, next) {
-        var db, comments;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-                switch (_context.prev = _context.next) {
-                    case 0:
-                        _context.prev = 0;
-                        _context.next = 3;
-                        return dbPromise;
+_nunjucks2.default.configure('views', {
+    autoescape: true,
+    express: app,
+    noCache: true,
+    watch: true
+});
 
-                    case 3:
-                        db = _context.sent;
-                        _context.next = 6;
-                        return db.all('SELECT \n        COMMENTS.id as id,\n            USERS.name AS author,\n            COMMENTS.message AS message \n        FROM COMMENTS \n        INNER JOIN USERS on USERS.id = COMMENTS.use');
+app.use(_bodyParser2.default.urlencoded());
 
-                    case 6:
-                        comments = _context.sent;
+async function initDb() {
+    const db = await dbPromise;
+    await db.run(`PRAGMA foreign_keys = ON;`);
+    return db;
+}
 
-                        res.send(comments);
-                        _context.next = 13;
-                        break;
+app.get('/', async (req, res, next) => {
+    try {
+        const db = await initDb();
+        const comments = await db.all(`SELECT 
+                COMMENTS.id as id,
+                USERS.name AS author,
+                COMMENTS.message AS message 
+            FROM COMMENTS 
+            INNER JOIN USERS on USERS.id = COMMENTS.user`);
+        console.log(comments.length);
+        res.render('comments.njk', { comments });
+    } catch (err) {
+        next(err);
+    }
+});
+app.post('/comment', async (req, res, next) => {
+    console.log("user", req.body.userid);
+    console.log("message", req.body.message);
+    try {
+        const db = await initDb();
+        const result = await db.run(`
+        INSERT INTO "COMMENTS" (
+            user,
+            message
+        )
+        VALUES (
+            $user,
+            $message
+        );`, {
+            $user: req.body.userid,
+            $message: req.body.message
+        });
+        // console.log("result", result);
+        res.redirect('/');
+    } catch (err) {
+        next(err);
+    }
+});
 
-                    case 10:
-                        _context.prev = 10;
-                        _context.t0 = _context['catch'](0);
-
-                        next(_context.t0);
-
-                    case 13:
-                    case 'end':
-                        return _context.stop();
-                }
-            }
-        }, _callee, undefined, [[0, 10]]);
-    }));
-
-    return function (_x, _x2, _x3) {
-        return _ref.apply(this, arguments);
-    };
-}());
-
-app.listen(8081);
+app.enable('trust proxy');
+app.listen(8000);
